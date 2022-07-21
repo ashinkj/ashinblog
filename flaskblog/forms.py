22214a60309de2ac4de590efa4a0576e1684f6d4
Ashin K J <ashinkj06@gmail.com>
2022-07-21 22:49:08 +0530

    post delete,update added

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -3,7 +3,8 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField,FileAllowed
 from flask_login import current_user
 from jsonschema import ValidationError
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from matplotlib.pyplot import title
+from wtforms import StringField, PasswordField, SubmitField, BooleanField,TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
 from flaskblog.models import User
 
@@ -58,3 +59,8 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken.Please choose a different one.')
+
+class PostForm(FlaskForm):
+    title=StringField('Title',validators=[DataRequired()])
+    content=TextAreaField('Content',validators=[DataRequired()])
+    submit=SubmitField('Post')
